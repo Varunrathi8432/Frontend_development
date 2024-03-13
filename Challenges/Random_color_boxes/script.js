7351706d39f7eb498beb67205a5d764356a0d098
Varunrathi8432 <varunrathi8432@gmail.com>
2024-03-14 01:05:51 +0530

    Challenges

---
+++ Challenges/Random_color_boxes/script.js
@@ -0,0 +1,12 @@
+let boxes=document.getElementsByClassName("box");
+function color(){
+    let val1=Math.ceil(0+Math.random()*255);
+    let val2=Math.ceil(0+Math.random()*255);
+    let val3=Math.ceil(0+Math.random()*255);
+    return `rgb(${val1},${val2},${val3})`;
+}
+
+Array.from(boxes).forEach(t=>{
+    t.style.backgroundColor=color();
+    t.style.color=color();
+})
